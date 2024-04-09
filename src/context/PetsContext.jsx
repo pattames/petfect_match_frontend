bde325908b36abe0dac6b0fa6dda160594fce283
Patricio <patricio@Tames-MacBook-Pro.local>
2024-04-09 16:25:50 +0200

    for andrea

---
+++ src/context/PetsContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useEffect, useState } from "react";
+
+export const PetsContext = createContext();
+
+export default function PetsContextProvider(props) {
+  const [pets, setPets] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(
+          //https://purrfect-backend-hsd1.onrender.com/pets
+          "http://localhost:8080/pets/"
+        );
+        const data = await res.json();
+        setPets(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  //We pass the data through "value":
+  return (
+    <PetsContext.Provider value={{ pets, setPets }}>
+      {props.children}
+    </PetsContext.Provider>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import UserContextProvider from "./context/UserContext.jsx";
+import PetsContextProvider from "./context/PetsContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PetsContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PetsContextProvider>
     </UserContextProvider>
   </React.StrictMode>
 );
