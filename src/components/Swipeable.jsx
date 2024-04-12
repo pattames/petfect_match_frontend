a1a4f9297aa72707c4b0e259cf555f6e3c96d961
Marco Fanti <fanti.marco8@gmail.com>
2024-04-12 14:16:06 +0200

    filtering buttons in swipeable component

--- src/components/Swipeable.jsx
+++ src/components/Swipeable.jsx
@@ -1,12 +1,36 @@
-
 import { PetsContext } from "../context/PetsContext";
-import { useContext, useState } from "react";
+import { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Swipeable() {
   const { pets } = useContext(PetsContext);
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const [filteredPets, setFilteredPets] = useState(pets);
+
+  useEffect(() => {
+    setFilteredPets(pets);
+  }, [pets]);
+
+  const dogs = pets && pets.filter((pet) => pet.pet_type === "dog");
+  const cats = pets && pets.filter((pet) => pet.pet_type === "cat");
+  const others = pets && pets.filter((pet) => pet.pet_type === "other");
+
+  const handleAllPets = () => {
+    setFilteredPets(pets);
+  };
+
+  const handleDogs = () => {
+    setFilteredPets(dogs);
+  };
+
+  const handleCats = () => {
+    setFilteredPets(cats);
+  };
+
+  const handleOthers = () => {
+    setFilteredPets(others);
+  };
   const handleNext = () => {
     if (currentIndex === pets.length - 1) {
       return;
@@ -21,14 +45,18 @@ function Swipeable() {
     setCurrentIndex((prev) => prev - 1);
   };
 
-  const currentPet = pets && pets[currentIndex];
+  const currentPet = filteredPets && filteredPets[currentIndex];
 
   return (
     <div>
+      <button onClick={handleAllPets}>All</button>
+      <button onClick={handleDogs}>Dogs</button>
+      <button onClick={handleCats}>Cats</button>
+      <button onClick={handleOthers}>Others</button>
       {currentIndex < pets?.length && (
         <img
-          src={pets[currentIndex].images[0].url}
-          alt={pets[currentIndex].name}
+          src={currentPet && currentPet.images[0].url}
+          alt={currentPet && currentPet.name}
         />
       )}
       <Link to={`/match/${currentPet._id}`}>
@@ -38,7 +66,6 @@ function Swipeable() {
       <button onClick={handleNext}>Next</button>
       <button onClick={handleBack}>Back</button>
     </div>
-
   );
 }
 
