48be193d9dbc1f1a5f5af7a278ea6462dd09c9b8
Tamés <patriciotames@Patricios-Mac-mini.local>
2024-04-12 10:50:21 +0200

    preferences structure + deleting swipable

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -1,9 +1,11 @@
+import Preferences from "./Preferences";
 import Swipeable from "./Swipeable";
 
 function Match() {
   console.log("Match");
   return (
     <div>
+      <Preferences />
       <Swipeable />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import UserContextProvider from "./context/UserContext.jsx";
 import PetsContextProvider from "./context/PetsContext.jsx";
+import PreferencesContextProvider from "./context/PreferencesContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <PetsContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PetsContextProvider>
-    </UserContextProvider>
+    <PreferencesContextProvider>
+      <UserContextProvider>
+        <PetsContextProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PetsContextProvider>
+      </UserContextProvider>
+    </PreferencesContextProvider>
   </React.StrictMode>
 );
