c24177ec73c2990174ef65d29932d98f1eda5eba
Marco Fanti <fanti.marco8@gmail.com>
2024-04-15 17:46:28 +0200

    style match page and error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,5 +1,18 @@
 function ErrorPage() {
-  return <div>Error 404</div>;
+  return (
+    <>
+      <h1 style={{ textAlign: "center", marginTop: "3rem" }}>
+        Something went wrong!
+      </h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img
+          style={{ width: "70rem", height: "50rem" }}
+          src="/error404.jpeg"
+          alt=""
+        />
+      </div>
+    </>
+  );
 }
 
 export default ErrorPage;
--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -4,10 +4,35 @@ import styles from "../css/Match.module.css";
 
 function Match() {
   return (
-    <div className={styles.container}>
-      <PreferencesForm />
-      <Swipeable />
-    </div>
+    <>
+      <div className={styles.main_container}>
+        <div className={styles.title}>
+          <img className={styles.img} src="/instpic.jpg" alt="" />
+        </div>
+        <div className={styles.instructions_box}>
+          <div className={styles.instructions_title}> Instructions</div>
+          <div className={styles.instructions_sentence}>
+            Unleash your inner animal lover by clicking on all the pet types
+            first
+          </div>
+          <div className={styles.instructions_sentence}>
+            Select your pet preferences (age, size and gender) and be ready to
+            meet your new furry BFF!
+          </div>
+          <div className={styles.instructions_sentence}>
+            Looking for endless snuggles or a playful adventure buddy? Scroll
+            with the buttons and discover...
+          </div>
+          <h2 className={styles.instructions_match}>
+            ...if it's a PetFect Match or not!
+          </h2>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <PreferencesForm />
+        <Swipeable />
+      </div>
+    </>
   );
 }
 
