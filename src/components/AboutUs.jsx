311c93a844aa4505534e000d6844c155bee4630e
Niloufar Asghari <nilou.asghari@gmail.com>
2024-04-17 12:26:28 +0200

    about page and animation and landing page button

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,19 +6,27 @@ function AboutUs() {
       <h1 className={styles.heading}>Meet the Development Team</h1>
       <div className={styles.gridContainer}>
         <div className={styles.gridItem}>
-          <img src="image1.jpg" alt="Image 1" className={styles.image} />
+          <div className={styles.imgContainer}>
+            <img src="marco.jpeg" alt="marco" className={styles.image} />
+          </div>
           <h2 className={styles.title}>Marco</h2>
         </div>
         <div className={styles.gridItem}>
-          <img src="Niloufar.jpeg" alt="Image 2" className={styles.image} />
+          <div className={styles.imgContainer}>
+            <img src="Niloufar.jpeg" alt="Niloufar" className={styles.image} />
+          </div>
           <h2 className={styles.title}>Niloufar</h2>
         </div>
         <div className={styles.gridItem}>
-          <img src="Andrea.JPG" alt="Image 3" className={styles.image} />
+          <div className={styles.imgContainer}>
+            <img src="Andrea.JPG" alt="Andrea" className={styles.image} />
+          </div>
           <h2 className={styles.title}>Andrea</h2>
         </div>
         <div className={styles.gridItem}>
-          <img src="image4.jpg" alt="Image 4" className={styles.image} />
+          <div className={styles.imgContainer}>
+            <img src="patricio.jpg" alt="patricio" className={styles.image} />
+          </div>
           <h2 className={styles.title}>Patricio</h2>
         </div>
       </div>
