b386ef6eed92b8d393d9df1be15ca3ba35ea7b0c
Tamés <patriciotames@patricios-mini.speedport.ip>
2024-06-12 11:24:14 +0200

    homepage spinner done

--- src/components/LandingSwipe.jsx
+++ src/components/LandingSwipe.jsx
@@ -8,41 +8,45 @@ import "swiper/css/navigation"; // Import CSS for navigation arrows
 import "swiper/css/effect-cards";
 import styles from "../css/LandingSwipe.module.css"; // Import CSS module
 import { EffectCards, Pagination, Navigation } from "swiper/modules";
+import MoonLoader from "react-spinners/MoonLoader";
 
 export default function LandingSwipe() {
   const { pets } = useContext(PetsContext);
 
   return (
     <div className={styles.container}>
-      <Swiper
-        effect={"cards"} // Add the effect property to enable the tilting effect
-        grabCursor={true}
-        modules={[EffectCards, Pagination, Navigation]}
-        pagination={true} // Enable pagination dots
-        navigation={false} // Enable navigation arrows
-        className={styles.mySwiper}
-        style={{
-          "--swiper-navigation-color": "#000",
-          "--swiper-pagination-color": "#dedede",
-        }} // Use CSS module class
-      >
-        {/* Map through each pet and display the first image in each slide */}
-        {pets &&
-          pets.map((pet, index) => (
-            <SwiperSlide key={index} className={styles.slide}>
-              <div className={styles.petCard}>
-                <img
-                  src={pet.images && pet.images[0] && pet.images[0].url}
-                  alt={`pets ${index + 1}`}
-                  className={styles.image}
-                />
-                <div className={styles.nameWrapper}>
-                  <h3>{pet.name}</h3>
+      {pets === "" ? (
+        <MoonLoader size={100} cssOverride={{ margin: "5rem" }} />
+      ) : (
+        <Swiper
+          effect={"cards"} // Add the effect property to enable the tilting effect
+          grabCursor={true}
+          modules={[EffectCards, Pagination, Navigation]}
+          pagination={true} // Enable pagination dots
+          navigation={false} // Enable navigation arrows
+          className={styles.mySwiper}
+          style={{
+            "--swiper-navigation-color": "#000",
+            "--swiper-pagination-color": "#dedede",
+          }} // Use CSS module class
+        >
+          {pets &&
+            pets.map((pet, index) => (
+              <SwiperSlide key={index} className={styles.slide}>
+                <div className={styles.petCard}>
+                  <img
+                    src={pet.images && pet.images[0] && pet.images[0].url}
+                    alt={`pets ${index + 1}`}
+                    className={styles.image}
+                  />
+                  <div className={styles.nameWrapper}>
+                    <h3>{pet.name}</h3>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          ))}
-      </Swiper>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      )}
       <div className={styles.btnWrapper}>
         <Link to="/match">
           <button className={styles.btn}>Find your match</button>
